072e66e06be8c1467bce8dacebbd9327c15fe45d
Asrat <asratextras77@gmail.com>
2025-12-29 23:19:13 +0300

    Add ngrok-skip-browser-warning header to API requests

--- client/components/auth/TelegramLoginButton.tsx
+++ client/components/auth/TelegramLoginButton.tsx
@@ -95,6 +95,7 @@ export default function TelegramLoginButton({
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
+                    'ngrok-skip-browser-warning': 'true',
                 },
                 body: JSON.stringify(authData),
             });
