e326908f800bf6cae43019694881b92367221122
Asrat <asratextras77@gmail.com>
2025-12-29 23:25:28 +0300

    Allow Netlify and ngrok origins in CORS configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins [ "http://localhost:3000", "https://bellasports.com" ]
+    origins [ 
+      "http://localhost:3000", 
+      "http://localhost:3001",
+      "https://bellasports.com",
+      "https://superlative-dragon-ceecc3.netlify.app",
+      /\Ahttps:\/\/.*\.ngrok-free\.app\z/
+    ]
 
     resource "*",
       headers: :any,
